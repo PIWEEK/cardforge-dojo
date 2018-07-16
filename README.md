b2317f23552f63dda59a7349079266d938f010a5
alonso.torres <alonso.torres@kaleidos.net>
2018-07-16 10:27:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CARD DOJO
+
+PIWEEK Project
+
+## Technology
+
+- ThreeJS
+
+## TODO
+
+- [x] Basic webpack configuration and ThreeJS "hello world"
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const port = 2222;
+
+module.exports = {
+  mode: 'development',
+
+  resolve: {
+    extensions: ['.js'],
+    modules: ['node_modules', 'src']
+  },
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:' + port,
+    'webpack/hot/only-dev-server',
+    'index.js'
+  ],
+
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    hot: true,
+    port: port,
+    //stats: {
+    //    colors: true,
+    //    hash: false,
+    //    version: false,
+    //    timings: false,
+    //    assets: false,
+    //    chunks: false,
+    //    modules: false,
+    //    reasons: false,
+    //    children: false,
+    //    source: false,
+    //    errors: true,
+    //    errorDetails: false,
+    //    warnings: true,
+    //    publicPath: false
+    //}
+  },
+
+  devtool: 'eval-source-map',
+
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.ProvidePlugin({
+      'THREE': 'three'
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+          'source-map-loader'
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
+      }
+    ]
+  }
+}
